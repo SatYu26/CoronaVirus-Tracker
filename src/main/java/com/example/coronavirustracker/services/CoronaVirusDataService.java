fc60b7918b03e450c5c14802b56836be6ebd1ea1
SatYu26 <satyamgoyal008@gmail.com>
2022-02-23 02:50:53 +0530

    Service and Model Code: Created the model for the Location data we are getting from csv, also managed to parse that data properly and extract out required information only and print out in toString

--- src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,6 +1,6 @@
 package com.example.coronavirustracker.services;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
+import com.example.coronavirustracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,15 +13,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+        // for avoiding concurruncy we will populate this new
+        // List  first and after that shift data to the output Array.
+        List<LocationStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
@@ -32,8 +39,13 @@ public class CoronaVirusDataService {
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationStat);
+            newStats.add(locationStat);
         }
+        this.allStats = newStats;
     }
 }
