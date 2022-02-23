358aa0a2bcb6f5f0c9e792efcb020b0e83aa4ecb
SatYu26 <satyamgoyal008@gmail.com>
2022-02-24 02:57:59 +0530

    adding the UI and making data render into it. Project completed, will start with Unit Tests

--- src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
@@ -21,14 +21,23 @@ public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    // Main Array that will contain all the parsed and cleaned data
     private List<LocationStats> allStats = new ArrayList<>();
 
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
-    @Scheduled(cron = "* * 1 * * *")
+    @Scheduled(cron = "0 0 12 * * ?")
     public void fetchVirusData() throws IOException, InterruptedException {
-        // for avoiding concurruncy we will populate this new
+
+        // for avoiding concurrency we will populate this new
         // List  first and after that shift data to the output Array.
+
         List<LocationStats> newStats = new ArrayList<>();
+
+        // Creating a new Http client and sending the request to fetch the data and storing it.
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
@@ -36,16 +45,21 @@ public class CoronaVirusDataService {
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 //        System.out.println(httpResponse.body());
 
+        // Parsing the CSV data that we are fetching
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        // Iterating over parsed data to extract only useful information and storing in locationStat.
         for (CSVRecord record : records) {
             LocationStats locationStat = new LocationStats();
-            locationStat.setState(record.get("Province/State"));
-            locationStat.setCountry(record.get("Country/Region"));
-            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
-            System.out.println(locationStat);
-            newStats.add(locationStat);
+            locationStat.setState(record.get("Province/State")); // getting data related to state.
+            locationStat.setCountry(record.get("Country/Region")); // getting data related to countries.
+            int latestCases = Integer.parseInt(record.get(record.size() - 1));
+            int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+            newStats.add(locationStat); // appending the data of individual state/country to newState array.
         }
-        this.allStats = newStats;
+        this.allStats = newStats; // finally, copying the data to output array.
     }
 }
